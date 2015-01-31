eb43406e464fa4bcd72073fff769d254fcf41bc8
trixpama <trixpama@gmail.com>
2015-01-30 18:04:10 -0800

    Revert ddfa7bc..3b2643a
    This rolls back to commit ddfa7bc14c65fe9dcc20e122d25e8228e65f9dbe.

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 // script.js
 
-    // create the module and name it scotchApp
-        // also include ngRoute for all our routing needs
+// create the module and name it scotchApp
+    // also include ngRoute for all our routing needs
 
 //    var scotchApp =  angular.module('myApp', ['jdFontselect']).constant('jdFontselectConfig', {
 //   googleApiKey: 'AIzaSyBylGW2HEJZdkYSLe-r1oyTncoZUXCnqIA'
 // });    
-<<<<<<< HEAD
 var scotchApp = angular.module('scotchApp', ['ngRoute']);
 
 // configure our routes
@@ -25,17 +24,17 @@ scotchApp.config(function($routeProvider) {
             controller  : 'aboutController'
         })
 
-        // route for the primary font list
-        .when('/primaryFont', {
-            templateUrl : 'pages/primaryFont.html',
-            controller  : 'primaryFontController'
+        // route for the contact page
+        .when('/contact', {
+            templateUrl : 'pages/contact.html',
+            controller  : 'contactController'
         })
     
-        // // route for the contact page
-        // .when('/font/:serifs', {
-        //     templateUrl : 'pages/primaryFont.html',
-        //     controller  : 'primaryFontController'
-        // });
+        // route for the contact page
+        .when('/font/:font', {
+            templateUrl : 'pages/contact.html',
+            controller  : 'contactController'
+        });
 });
 
 
@@ -56,43 +55,13 @@ $.getJSON(url, function(res) {
 
     var sansSerifs = _.filter(res.items, function(item) {
         return item.category === "sans-serif";
-=======
-    var scotchApp = angular.module('scotchApp', ['ngRoute']);
-
-    // configure our routes
-    scotchApp.config(function($routeProvider) {
-        $routeProvider
-
-            // route for the home page
-            .when('/', {
-                templateUrl : 'pages/home.html',
-                controller  : 'mainController'
-            })
-
-            // route for the about page
-            .when('/about', {
-                templateUrl : 'pages/about.html',
-                controller  : 'aboutController'
-            })
-
-            // route for the contact page
-            .when('/contact', {
-                templateUrl : 'pages/contact.html',
-                controller  : 'contactController'
-            });
->>>>>>> parent of ddfa7bc... matt touched it......
     });
 
-    // // create the controller and inject Angular's $scope
-    // scotchApp.controller('mainController', function($scope) {
-    //     // create a message to display in our view
-    //     $scope.message = 'Everyone come and see how good I look!';
-    // });
+    var museos = _.filter(res.items, function(item) {
+        return item.family.toLowerCase() === "anonymous pro";
+    });
 
-    // scotchApp.controller('aboutController', function($scope) {
-    //     $scope.message = 'Look! I am an about page.';
-    // });
 
-    // scotchApp.controller('contactController', function($scope) {
-    //     $scope.message = 'Contact us! JK. This is just a demo.';
-    // });
+    console.log(museos);
+});
+*/
