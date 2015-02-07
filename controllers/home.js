f3196149c0689104b129333b6c7c015757489e20
kennethlai21 <kla83@sfu.ca>
2015-02-06 21:13:25 -0800

    i think we are done....
    omg..

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,32 @@
     
         // create the controller and inject Angular's $scope
-    scotchApp.controller('mainController', function($scope) {
+    scotchApp.controller('mainController', function($scope, $route) {
         // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
+
+        var font = decodeURIComponent($route.current.params.font);
         
+        console.log(font);
+        
+        if (font) {
+            
+            //this should be in another controller with another view
+            
+            var pairs = {
+                'fontSerif':[
+                    {name:'Roboto Slab', cssName:'font-roboto-slab'},
+                    {name:'EB Garamond', cssName:'font-eb-garamond'},
+                    {name:'Noto Serif', cssName:'font-noto-serif'},
+                    {name:'PT Serif', cssName:'font-pt-serif'},
+                    {name:'Quattrocento', cssName:'font-quattrocento'}
+                ],
+                'fontSansSerif':[
+                    {name:'Open Sans', cssName:'font-open-sans'},
+                    {name:'Lato', cssName:'font-lato'},
+                    {name:'Roboto', cssName:'font-roboto'},
+                    {name:'Raleway', cssName:'font-raleway'},
+                    {name:'PT Sans', cssName:'font-pt-sans'}
+                ]
+            }; 
         $scope.favorites = scotchApp.favorites;
+        }
     });
